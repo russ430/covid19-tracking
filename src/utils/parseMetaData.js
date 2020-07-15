f2a42b97d56403149dc2672f8022826865d6bcd5
Alex <alexjrussian@gmail.com>
2020-07-15 16:26:02 -0400

    refactored for restructured meta data shape

--- src/utils/parseMetaData.js
+++ src/utils/parseMetaData.js
@@ -1,10 +1,9 @@
+/* eslint-disable no-param-reassign */
 export default function parseMetaData(data) {
-  const meta = {};
-  data.forEach((state) => {
-    const abbreviation = state.state.toLowerCase();
-    if (!meta[abbreviation]) {
-      meta[abbreviation] = { ...state, state: abbreviation };
-    }
+  const meta = data.filter((state) => state.name !== 'American Samoa');
+  meta.forEach((state) => {
+    state.state = state.state.toLowerCase();
   });
+  meta.sort((a, b) => (a.name > b.name ? 1 : -1));
   return meta;
 }
