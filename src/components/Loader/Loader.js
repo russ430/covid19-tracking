f1907101b3b3bcbfc857962437ad82a150fd85e9
Alex <alexjrussian@gmail.com>
2020-07-16 15:37:55 -0400

    created a Loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Load = styled.div`
+  border-radius: 50%;
+  width: 1em;
+  height: 1em;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation: load7 1.8s infinite ease-in-out;
+  animation: load7 1.8s infinite ease-in-out;
+  color: #000000;
+  font-size: 10px;
+  margin: 0 auto;
+  position: relative;
+  text-indent: -9999em;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    border-radius: 50%;
+    width: 1em;
+    height: 1em;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+    -webkit-animation: load7 1.8s infinite ease-in-out;
+    animation: load7 1.8s infinite ease-in-out;
+  }
+
+  &:before {
+    left: -1.5em;
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+  }
+
+  &:after {
+    left: 1.5em;
+  }
+
+  @-webkit-keyframes load7 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 1em 0 -0.5em;
+    }
+    40% {
+      box-shadow: 0 1em 0 0;
+    }
+  }
+  @keyframes load7 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 1em 0 -0.5em;
+    }
+    40% {
+      box-shadow: 0 1em 0 0;
+    }
+`;
+
+export default function Loader() {
+  return <Load />;
+}
