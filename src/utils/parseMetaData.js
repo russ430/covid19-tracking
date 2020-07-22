27fa22b3aa5f6bba93d9cea74019fe60ede4c64f
Alex <alexjrussian@gmail.com>
2020-07-22 17:12:12 -0400

    refactored to include more data

--- src/utils/parseMetaData.js
+++ src/utils/parseMetaData.js
@@ -1,8 +1,15 @@
-/* eslint-disable no-param-reassign */
 export default function parseMetaData(data) {
-  const meta = [...data];
-  meta.forEach((state) => {
-    state.state = state.state.toLowerCase();
+  const meta = data.map((state) => {
+    return {
+      state: state.state.toLowerCase(),
+      name: state.name,
+      id: state.fips,
+      covid19Site: state.covid19Site,
+      covid19SiteSecondary: state.covid19SiteSecondary,
+      covid19SiteTertiary: state.covid19SiteTertiary,
+      twitterHandle: state.twitter,
+      notes: state.notes,
+    };
   });
   meta.sort((a, b) => (a.name > b.name ? 1 : -1));
   return meta;
