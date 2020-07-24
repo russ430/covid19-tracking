3da369b3719a51b442cd21bb218243af8272716f
Alex <alexjrussian@gmail.com>
2020-07-23 21:33:05 -0400

    delete xtra files, added tests for current reducer

---
+++ src/redux/__tests__/reducers/currentData.test.js
@@ -0,0 +1,48 @@
+import reducer, { initialState } from '../../reducers/currentData';
+import {
+  getCurrentStateData,
+  getCurrentStateDataSuccess,
+  getCurrentStateDataFailure,
+} from '../../actions/actions';
+
+describe('currentData reducer', () => {
+  it('given undefined returns initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('handles GET_CURRENT_STATE_DATA', () => {
+    expect(reducer(initialState, getCurrentStateData())).toEqual({
+      ...initialState,
+      isFetching: true,
+      error: null,
+    });
+  });
+
+  it('handles GET_CURRENT_STATE_DATA_SUCCESS', () => {
+    const data = 1;
+    expect(
+      reducer(
+        { ...initialState, isFetching: true },
+        getCurrentStateDataSuccess(data),
+      ),
+    ).toEqual({
+      ...initialState,
+      isFetching: false,
+      data,
+    });
+  });
+
+  it('handles GET_CURRENT_STATE_DATA_FAILURE', () => {
+    const error = 1;
+    expect(
+      reducer(
+        { ...initialState, isFetching: true },
+        getCurrentStateDataFailure(error),
+      ),
+    ).toEqual({
+      ...initialState,
+      isFetching: false,
+      error,
+    });
+  });
+});
