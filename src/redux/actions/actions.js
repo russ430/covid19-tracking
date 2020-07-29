fcf774d703878c4a73ac2e9e3d8d7df82a97be70
Alex <alexjrussian@gmail.com>
2020-07-29 13:39:21 -0400

    remove console.log

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -141,7 +141,6 @@ export const requestStateDailyData = (state) => {
         dispatch(getDailyDataSuccess(parsed));
       })
       .catch((error) => {
-        console.log(error);
         dispatch(getDailyDataFailure(error));
       });
   };
