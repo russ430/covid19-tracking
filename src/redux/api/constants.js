25eb1407029898f18b897c252be6b1675ea0402c
Alex <alexjrussian@gmail.com>
2020-08-19 10:15:55 -0400

    updated api URLs

--- src/redux/api/constants.js
+++ src/redux/api/constants.js
@@ -1,13 +1,13 @@
 export const ALL_STATES_CURRENT_DATA =
-  'https://covidtracking.com/api/v1/states/current.json';
+  'https://api.covidtracking.com/v1/states/current.json';
 
 export const STATE_DAILY_DATA = (state) =>
-  `https://covidtracking.com/api/v1/states/${state}/daily.json`;
+  `https://api.covidtracking.com/v1/states/${state}/daily.json`;
 
 export const ALL_STATES_META =
-  'https://covidtracking.com/api/v1/states/info.json';
+  'https://api.covidtracking.com/v1/states/info.json';
 
-export const US_DAILY_DATA = 'https://covidtracking.com/api/v1/us/daily.json';
+export const US_DAILY_DATA = 'https://api.covidtracking.com/v1/us/daily.json';
 
 export const CDC_RESOURCES =
   'https://tools.cdc.gov/api/v2/resources/media/403372.rss';
